1a72409d47044f24a530954d9b8173ab6bb39bcf
Husanpreet Singh <167855282+sainihusan@users.noreply.github.com>
2026-05-08 12:41:40 +0530

    Update mailer.js

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -1,9 +1,13 @@
 const nodemailer = require("nodemailer");
+const dns = require("dns");
+
+dns.setDefaultResultOrder("ipv4first");
 
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT),
   secure: false,
+  family: 4, // force IPv4
 
   auth: {
     user: process.env.SMTP_USER,
